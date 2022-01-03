deac1d8b46d5ff1161fe9275a8fd2d68370279c6
Jon Garcia Tamayo <Jonmuxupvp@gmail.com>
2022-01-03 18:50:00 +0100

    Creado utilidades.js

---
+++ scripts/utilidades.js
@@ -0,0 +1,120 @@
+
+class ManejadorDeEscenas {
+    constructor(escenas) {
+        this.escenas = escenas;
+        this.escenaActual = this.escenas[0];
+    }
+
+    cambiarEscena(indice) {
+        this.escenaActual = this.escenas[indice];
+    }
+
+    listarEscenas() {
+        console.table(this.escenas);
+    }
+}
+
+class Escena {
+    constructor(id, imagen, objetos) {
+        this.id = id;
+        this.imagen = imagen;
+        this.objetos = objetos;
+    }
+}
+
+class Objeto {
+    constructor(area, juego, codigo) {
+        this.area = area;
+        this.juego = juego;
+        this.codigo = codigo;
+    }
+}
+
+class Area {
+    constructor(minX, minY, maxX, maxY) {
+        this.minX = minX;
+        this.minY = minY;
+        this.maxX = maxX;
+        this.maxY = maxY;
+    }
+
+    enArea(x, y) {
+        return (x > this.minX && x < this.maxX) && (y > this.minY && y < this.maxY);
+    }
+}
+
+class ManejadorDeFlags {
+    constructor(flags) {
+        this.flags = flags;
+    }
+
+    getFlag(indice) {
+        return this.flags[indice];
+    }
+
+    listarFlags() {
+        console.table(this.flags);
+    }
+}
+
+class Flag {
+    constructor(id, activa = false) {
+        this.id = id;
+        this.activa = activa;
+    }
+
+    activar() {
+        this.activa = true;
+    }
+
+    desactivar() {
+        this.activa = false;
+    }
+}
+
+class ManejadorDeDialogos {
+    constructor(dialogos) {
+        this.dialogos = dialogos;
+    }
+
+    getDialogo(indice) {
+        return this.dialogos[indice];
+    }
+}
+
+class ManejadorDePistaSonido {
+    constructor(pistas, sonidos) {
+        this.pistas = pistas;
+        this.sonidos = sonidos;
+    }
+
+    playPista(indice) {
+        this.pistas[indice].play();
+    }
+
+    pausaPista(indice) {
+        this.pistas[indice].pause();
+    }
+
+    buclePista(indice) {
+        this.pistas[indice].loop = true;
+    }
+
+    playSonido(indice) {
+        this.sonidos[indice].play();
+    }
+
+    listarPistas() {
+        console.table(this.pistas);
+    }
+
+    listarSonidos() {
+        console.table(this.sonidos);
+    }
+}
+
+class Texto {
+    constructor(texto) {
+        this.texto = texto
+    }
+}
